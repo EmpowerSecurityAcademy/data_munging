6c0b720cd5913602c6d2ef8cd0f63291833c4003
sheltowt <sheltowt@gmail.com>
2016-08-12 15:34:17 -0400

    updates

--- scripts/group_by.py
+++ scripts/group_by.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+departments = pd.read_csv("../data/departments.csv")
+dept_emp = pd.read_csv("../data/dept_emp.csv")
+
+merged = pd.merge(departments, dept_emp, on='dept_no')
+
+aggregations = {
+	
+}
+
+merged[merged['item'] == 'call'].groupby('').agg(aggregation)
+
+merged.to_csv("../example/group_by.csv")
